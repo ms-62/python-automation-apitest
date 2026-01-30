2462f7b1057b321e88cfe1ef74fcc9de91f22aff
ms-62 <misol9462@gmail.com>
2026-01-30 16:41:16 +0900

    Add allure Report

--- README.md
+++ README.md
@@ -14,5 +14,23 @@ dot_clean .
 
 
 ### Next Step
-- HTTP Method newman test용 추가 작업
-- ci/cd 환경에서 api Test 돌릴 수 있게 yml 작업
+- HTTP Method newman test용 추가 작업 - (완료)
+- ci/cd 환경에서 api Test 돌릴 수 있게 yml 작업 
+
+## Report
+### pytest - allure 활용 (local)
+### 테스트 실핼 및 데이터 생성 
+### --alluredir을 통해 폴더 생성
+- python -m pytest --alluredir=allure-results
+### 결과 임시 서버로 띄워 브라우저 확인용 명령어
+- allure serve allure-results
+yml에도 적용 (gihub actions 생성확인) <br>
+
+### test 파일에 데코레이터로 설명 추가
+- @allure.feature()
+- @allure.story()
+- with allure.step():
+- allure.dynamic.description()
+
+### local reports 폴더는 .gitingore 추가
+
--- tests/test_simple_api.py
+++ tests/test_simple_api.py
@@ -1,3 +1,5 @@
+import allure
+import pytest
 
 
 def test_get_posts_list(api):
@@ -12,7 +14,9 @@ def test_get_post_list(api):
     assert response.status_code == 200  # 성공했는지 확인
     assert len(response.json()) > 0    # 데이터가 비어있지 않은지 확인
     print(f"\n 특정 사용자의 조회된 게시글 수: {len(response.json())}")
-
+    
+@allure.feature("게시글 관리")
+@allure.story("게시글 생성 테스트")
 def test_create_post_success(api): 
     payload = {
         "body": {
@@ -21,17 +25,19 @@ def test_create_post_success(api):
             "userId": 1
         }
     }
-    response = api.create_post(payload)
+    with allure.step("1. 게시글 데이터를 준비하고 전송합니다."):
+        response = api.create_post(payload)
     
-    assert response.status_code == 201  # 성공했는지 확인
-    assert len(response.json()) > 0    # 데이터가 비어있지 않은지 확인
-    return_data = response.json()
-    print(f"\n 등록 글 json: {response.json()}")
-    print(f"\n {return_data['body']}")
-    print(f"\n title : {return_data['body']['title']}")
-    print(f"\n body : {return_data['body']['body']}")
-    print(f"\n userId : {return_data['body']['userId']}")
-    
+    with allure.step("2. 응답 상태 코드가 201인지 확인합니다."):    
+        assert response.status_code == 201  # 성공했는지 확인
+        assert len(response.json()) > 0    # 데이터가 비어있지 않은지 확인
+        return_data = response.json()
+        print(f"\n 등록 글 json: {response.json()}")
+        print(f"\n {return_data['body']}")
+        print(f"\n title : {return_data['body']['title']}")
+        print(f"\n body : {return_data['body']['body']}")
+        print(f"\n userId : {return_data['body']['userId']}")
+    allure.dynamic.description("이 테스트는 게시글이 정상적으로 생성되는지 확인하는 시나리오입니다.")
 
 def test_patch_post_success(api):
     data = {
